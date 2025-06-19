e3148fe29e5e7ecfc787f92e03e6bf3a7d288fa7
illyasvelleri <cookon123@gmail.com>
2025-06-19 10:58:50 +0530

    date & time

--- models/Item.js
+++ models/Item.js
@@ -2,23 +2,29 @@ import mongoose from 'mongoose';
 
 const ItemSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  category: { 
-    type: String, 
-    enum: ['subjunior', 'junior', 'senior', 'general(individual)', 'general(group)'], 
-    required: true 
+  category: {
+    type: String,
+    enum: ['subjunior', 'junior', 'senior', 'general(individual)', 'general(group)'],
+    required: true
   },
-  type: { 
-    type: String, 
-    enum: ['A', 'B'], 
-    required: true 
+  type: {
+    type: String,
+    enum: ['A', 'B'],
+    required: true
   },
-  stage: { 
-    type: String, 
-    enum: ['stage', 'offstage'], 
-    required: true 
+  stage: {
+    type: String,
+    enum: ['stage', 'offstage'],
+    required: true
   },
   participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Contestant' }],
   isChecked: { type: Boolean, default: false },
+  date: { type: Date }, // Stores selected date
+  day: { type: String }, // Stores text day (e.g., "Monday")
+  timeRange: {
+    start: { type: String }, // e.g., "07:55"
+    end: { type: String }, // e.g., "08:00"
+  },
 });
 
 const Item = mongoose.models.Item || mongoose.model('Item', ItemSchema);
